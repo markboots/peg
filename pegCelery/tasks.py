9e84d49ab824db1db19192bce00e10510309722d
Mark Boots <mark.boots@usask.ca>
2012-07-15 00:04:57 -0600

    Reorganizes pegCelery files so that celery worker can be easily started using default loader: celeryconfig.py. Prepares to run as daemon.

--- pegCelery/tasks.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from pegCelery.celery import celery
-from subprocess import call
-
-
-@celery.task
-def runCalculation(argumentString):
-	return call("./pegSerial", argumentString.split())
